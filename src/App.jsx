84796c4d249950d22db93e021a4722c81799b004
boonboww <anhquan5504@gmail.com>
2024-12-19 09:03:12 +0700

    update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
-
+import Axios from 'axios'
+import { useState } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [cryptoList, setCryptoList] = useState([])
+  useEffect(() => {
+    Axios.get('https://api.coinlore.net/api/tickers/?start=0&limit=100').then((respone) => {
+      setCryptoList(respone.data["data"])
+      // console.log(respone.data["data"])
+    })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div id='header'>
+        <h1>CryptoLand</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='cryptoList'>
+        {cryptoList.map((coin) => {
+          return (
+            <div key={coin.id}>
+              <h1>{coin.symbol}</h1>
+              <h1>{coin.price_usd}</h1>
+            </div>
+          );
+        })}
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
